897673b0f027ded456c998db164fb72ae4625994
Sasanka742 <sasankap742@gmail.com>
2021-04-02 12:15:37 -0700

    *moved history from common to server

--- Common/src/main/java/edu/sdccd/cisc191/o/User.java
+++ Common/src/main/java/edu/sdccd/cisc191/o/User.java
@@ -2,9 +2,8 @@ package edu.sdccd.cisc191.o;
 
 public class User {
     private String userName;
-    History userHistory;
 
-    public void createDailyLog(){
+    public void editLog(){
 
     }
 
---
+++ Server/src/main/java/edu/sdccd/cisc191/o/server/History.java
@@ -0,0 +1,21 @@
+package edu.sdccd.cisc191.o.server;
+
+import edu.sdccd.cisc191.o.DailyLog;
+
+import java.util.ArrayList;
+
+public class History {
+    private ArrayList<DailyLog> logHistory;
+
+
+    public DailyLog getLog(){
+        return logHistory.get(0);
+    }
+    public void addEntry(){
+
+    }
+
+    public void viewLogs(){
+
+    }
+}
