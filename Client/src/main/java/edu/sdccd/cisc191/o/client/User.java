0fcd27d58cf8046603ce65325a2da34b46f7081a
alexpnguyen <alexngabcis@gmail.com>
2021-04-24 20:29:13 -0700

    Added password field, constructors, setters and getters. More comments are added for clarification. Networking not fully implemented yet.

--- Client/src/main/java/edu/sdccd/cisc191/o/client/User.java
+++ Client/src/main/java/edu/sdccd/cisc191/o/client/User.java
@@ -8,22 +8,70 @@ import java.io.*;
 
 public class User {
     private String userName;
+    private String password;
 
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
 
+    //Default constructor
+    public User() {
+        userName = "userName";
+        password = "password";
+    }
+
+    //Constructor
+    public User(String param_userName, String param_password) {
+        userName = param_userName;
+        password = param_password;
+    }
+
+    //Setter
+    public void setUserName(String param_userName) {
+        userName = param_userName;
+    }
+
+    //Setter
+    public void setPassword(String param_password) {
+        password = param_password;
+    }
+
+    //Getter
+    public String getUserName() {
+        return userName;
+    }
+
+    //Getter
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * startConnection() creates a connection with Server
+     * @param ip server ip
+     * @param port use 8888
+     * @throws IOException
+     */
     public void startConnection(String ip, int port) throws IOException {
-        clientSocket = new Socket(ip, port); //Client Socket created to given port&ip
-        out = new PrintWriter(clientSocket.getOutputStream(), true); //output from client to the other end of socket(server)
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //input from the other end of the socket(server)
+        clientSocket = new Socket(ip, port);    //Client Socket created to given port & ip
+        out = new PrintWriter(clientSocket.getOutputStream(), true);    //output from client to the other end of socket(server)
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  //input from the other end of the socket(server)
     }
 
+    /**
+     * sendRequest() sends a request to Server and returns a DailyLog
+     * @return DailyLog
+     * @throws Exception
+     */
     public DailyLog sendRequest() throws Exception {
-        out.println(Request.toJSON(new Request(1)));  //outputting to server a JSON serialized  object of Request
-        return DailyLog.fromJSON(in.readLine()); //outputting to server a JSON serialized  object of Request
+        out.println(Request.toJSON(new Request(1)));    //outputting to server a JSON serialized  object of Request
+        return DailyLog.fromJSON(in.readLine());    //outputting to server a JSON serialized  object of Request
     }
 
+    /**
+     * logout() closes all connections
+     * @throws IOException
+     */
     public void logout() throws IOException {
         //Close all the I/O streams and disconnect clientSocket
         in.close();
@@ -43,8 +91,9 @@ public class User {
         }
     }
 
-    public void editLog(){
 
+    public void editLog(){
+        //FIX ME
     }
 
 }
